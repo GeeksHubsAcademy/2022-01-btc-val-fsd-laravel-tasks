93f3b7e88f34050779106e7187615fbb91b832f6
Dani <d.tarazoma@geekshubs.com>
2022-03-30 14:38:50 +0200

    feat: model Task updated

--- blog/app/Models/Task.php
+++ blog/app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $table = 'tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'user_id'
+    ];
 }
